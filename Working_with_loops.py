69db8f00905a32f83a2f1c82297e2c5acaaae146
Alexander Skeries <aske725@gmail.com>
2025-01-23 17:32:14 -0500

    Guess the number!

---
+++ Working_with_loops.py
@@ -0,0 +1,15 @@
+#By convention, import statements are placed at the top of the script
+import random
+#Printing the game rules
+print("Welcome to Guess the Number!")
+print("The rules are simple. I will think of a number, and you will try to guess it.")
+#Handling game logic with w while loop
+number = random.randint(1,10)
+isGuessRight = False
+while isGuessRight != True:
+    guess = input("Guess a number between 1 and 10: ")
+    if int(guess) == number: 
+        print("You guessed {}. That is correct! You win!".format(guess))
+        isGuessRight = True
+    else:
+        print("You guessed {}. Sorry, that isn’t it. Try again.".format(guess))
