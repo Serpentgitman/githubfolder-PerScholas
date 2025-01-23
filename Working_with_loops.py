bb2c20749c8eca0547feb55fab30fbd0426ebc61
Alexander Skeries <aske725@gmail.com>
2025-01-23 17:37:29 -0500

    Guess the number!

--- Working_with_loops.py
+++ Working_with_loops.py
@@ -1,6 +1,6 @@
 #By convention, import statements are placed at the top of the script
 import random
-#Printing the game rules
+#Printing the game rules.
 print("Welcome to Guess the Number!")
 print("The rules are simple. I will think of a number, and you will try to guess it.")
 #Handling game logic with while loop
