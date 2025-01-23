13828a9ce50162c8ecabb0d68a1976e661b10fd9
Alexander Skeries <aske725@gmail.com>
2025-01-23 17:32:56 -0500

    Update Working_with_loops.py

--- Working_with_loops.py
+++ Working_with_loops.py
@@ -3,7 +3,7 @@ import random
 #Printing the game rules
 print("Welcome to Guess the Number!")
 print("The rules are simple. I will think of a number, and you will try to guess it.")
-#Handling game logic with w while loop
+#Handling game logic with while loop
 number = random.randint(1,10)
 isGuessRight = False
 while isGuessRight != True:
